21dbbd8758ae15e9dfdc29ffecfedf9689dab5d5
myhname <myh20011018>
2025-03-02 23:05:22 +0800

    指定检出目录尝试

--- src/router/index.ts
+++ src/router/index.ts
@@ -79,8 +79,7 @@ const constantsRoutes: Array<RouteRecordRaw> = [
 
 // tag: 静态路由：首页（带一个重定向的展示介绍界面），登录（封面和登录一个路由），个人中心，组件库，笔记库
 const router = createRouter({
-    // import.meta.env.BASE_URL
-    history: createWebHistory("/ScrewLake/"),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: constantsRoutes,
 })
 
