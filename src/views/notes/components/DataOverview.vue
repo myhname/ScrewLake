b96d5469e25e67b4234a533f2c144ffb2a71c3bb
myhname <myh20011018>
2025-03-03 22:06:29 +0800

    重建 notes

---
+++ src/views/notes/components/DataOverview.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="left-person-msg card-item" id="leftPersonMsg">
+    <div class="background-img">
+      <div class="name-and-signature">
+            <span class="user-name font-3">
+              {{ state.userName }}
+            </span>
+        <span class="user-signature font-5">
+              {{ state.userSignature }}
+            </span>
+      </div>
+    </div>
+    <div class="resource-tags">
+      <div class="resource-tag-item" v-for="(tagItem, tagIndex) in state.resourceTagsList" :key="tagIndex">
+        <span class="resource-tag-label font-3"> {{ tagItem.label }} </span>
+        <span class="resource-tag-number font-4"> {{ tagItem.value }} </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {reactive, onMounted} from "vue";
+import NoteData from "@/data/notesData.json"
+
+const state = reactive({
+  userName: "螺丝湖水怪之家",
+  userSignature: "时代的一粒沙，也想随风向天空去",
+  resourceTagsList: [
+    { label: "笔记", value: 0, },
+    { label: "随笔", value: 0, },
+    { label: "项目", value: 0, },
+    { label: "图片", value: 0, },
+  ] as Array<LabelValue>,
+})
+
+const getAll = () => {
+  state.resourceTagsList[0].value = (NoteData as Array<any>).length
+}
+
+onMounted(() => {
+  getAll()
+})
+
+
+</script>
+
+<style scoped lang="less">
+.left-person-msg {
+  width: 300px;
+  height: 300px;
+  border-radius: 5px;
+  overflow: hidden;
+
+  .background-img {
+    height: 80%;
+    background: url("../../../assets/img/yanhuanianjie.jpg") no-repeat center center / cover;
+    display: flex;
+    flex-direction: column-reverse;
+
+    .name-and-signature {
+      width: 100%;
+      height: 50px;
+      background-color: rgba(129, 119, 127, 0.60);
+      text-align: center;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      .user-name {
+        color: #f2ecec;
+      }
+
+      .user-signature {
+        color: #c8c8c8;
+        margin-top: 2px;
+      }
+    }
+  }
+
+  .resource-tags {
+    height: 20%;
+    display: flex;
+    flex-direction: row;
+    background-color: rgb(122 109 109 / 40%);
+    color: #e8e8e8;
+    position: relative;
+
+    .resource-tag-item {
+      height: 60%;
+      margin: auto;
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+      border-right: 1px solid #d3d3d3;
+
+      &:last-child {
+        border-right: none;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/notes/components/ShowNote.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="test-md-container">
+    <div class="note-container">
+      <!--        <md-preview-component class="left-preview-container"></md-preview-component>-->
+      <MdPreview class="left-preview-container" :editorId="id" :modelValue="markdown" :theme="'dark'"
+                 :previewTheme="'mk-cute'"/>
+
+      <div class="right-msg-container">
+        <data-overview></data-overview>
+        <div class="note-toc">
+          <el-scrollbar max-height="300px">
+            <MdCatalog :editorId="id" :scrollElement="scrollElement" @onClick="clickToc"/>
+          </el-scrollbar>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {computed, onMounted, ref} from 'vue'
+import {MdPreview, MdCatalog} from 'md-editor-v3';
+// preview.css相比style.css少了编辑器那部分样式
+import 'md-editor-v3/lib/preview.css';
+import DataOverview from "./DataOverview.vue"
+import {useRoute} from "vue-router";
+import {ElMessage} from "element-plus";
+// import MdPreviewComponent from "@/components/editors/MdPreviewComponent.vue";
+import emitter from '@/utils/eventBus'
+import NoteData from "@/data/notesData.json"
+import {fetchMarkdown} from "@/utils/readMdFile.ts"
+
+const route = useRoute()
+const id = 'preview-only';
+const scrollElement = document.documentElement;
+const markdown = ref<string>()
+const tocTitleDivList = ref<HTMLCollectionOf<HTMLElement>>()
+const currHeightTitle = ref(-1)
+
+const clickToc = (e: MouseEvent, t: any) => {
+  console.log("点击目录", e, t)
+  let PageId = document.getElementById(t.text)
+  console.log("dddd", PageId)
+  if (PageId) {
+    // window.scrollTo({
+    //   top: PageId.offsetTop,
+    //   behavior: "smooth"
+    // })
+    emitter.emit('clickNoteToc', PageId.offsetTop)
+  }
+
+//   高亮
+  if (!tocTitleDivList.value || !tocTitleDivList.value.length) {
+    tocTitleDivList.value = document.getElementsByClassName("md-editor-catalog-link")! as HTMLCollectionOf<HTMLElement>
+  }
+  console.log(("目录类"), tocTitleDivList.value)
+  console.log("字节点",)
+  if (currHeightTitle.value != -1) {
+    tocTitleDivList.value![currHeightTitle.value].children[0].classList.remove("highlight-title")
+  }
+  currHeightTitle.value = Number(t.index - 1)
+  tocTitleDivList.value![currHeightTitle.value].children[0].classList.add("highlight-title")
+
+}
+
+const getContext = async (id: string) => {
+
+  for (const item of (NoteData as Array<{ id: number, title: string }>)) {
+    if (item.id.toString() === id) {
+      console.log("文件路径：", `${import.meta.env.BASE_URL}notes/${item.title}.md`)
+      fetchMarkdown(item.title).then((res: any) => {
+        console.log("读取文件", res)
+        markdown.value = res
+      }).catch((err: any) => {
+        ElMessage.error(err)
+      }).finally(() => {
+      })
+    }
+  }
+}
+
+onMounted(() => {
+  if (route.query && route.query.id) {
+    getContext(String(route.query.id))
+    // view(Number(route.query.id))
+  }
+})
+
+</script>
+
+<style lang="less" scoped>
+.test-md-container {
+  position: relative;
+  width: 100%;
+  //padding: 20px 5% 20px 8%;
+  display: flex;
+
+  .note-container {
+    position: relative;
+    margin: 20px auto;
+    display: flex;
+    flex-direction: row;
+    column-gap: 20px;
+
+    .left-preview-container {
+      width: 70vw;
+    }
+
+    #preview-only {
+      background-color: rgba(0, 0, 0, 0.75);
+      border: 1px solid #e4e7ed;
+      border-radius: 5px;
+      box-shadow: 0 0 12px rgba(0, 0, 0, 0.12);
+      backdrop-filter: blur(10px);
+    }
+
+    .right-msg-container {
+      width: 300px;
+      display: flex;
+      flex-direction: column;
+      row-gap: 15px;
+
+      .note-toc {
+        position: sticky;
+        top: 10px;
+        padding: 10px;
+
+        color: #fff;
+        background-color: rgba(0, 0, 0, 0.45);
+        border: 1px solid #e4e7ed;
+        border-radius: 5px;
+        box-shadow: 0 0 12px rgba(0, 0, 0, 0.12);
+        backdrop-filter: blur(2px);
+      }
+    }
+  }
+}
+</style>
+
+<style lang="less">
+.highlight-title {
+  color: #01b4ff !important;
+}
+
+.md-editor-catalog-link span,
+.md-editor-copy-button,
+.md-editor-collapse-tips,
+.md-editor-code-head {
+  cursor: url("../../../assets/cursor/ani/Link.ani"), url("../../../assets/cursor/cur/cursorLink.cur"), pointer !important;
+}
+
+.md-editor-preview figure {
+  display: block;
+  margin: 10px auto;
+}
+
+#preview-only-preview {
+  padding: 0 20px;
+}
+</style>
